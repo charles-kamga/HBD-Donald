5ddd21e4d8ba32dade3580d282e9f52db6bde641
charles-kamga <charleskamgapr@gmail.com>
2025-12-10 17:48:55 +0100

    configuration firebase

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4CIlk1LB169YSBlp7bDehCAd8ysjpXFI",
+  authDomain: "hbd-donald.firebaseapp.com",
+  projectId: "hbd-donald",
+  storageBucket: "hbd-donald.appspot.com",
+  messagingSenderId: "534831777772",
+  appId: "1:534831777772:web:eea9ac14c02f6f47bce2c9",
+  measurementId: "G-BSWKMX37NM"
+};
